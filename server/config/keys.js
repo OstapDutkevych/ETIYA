e790b732409661a29fe4c678a8116251ff009c30
OstapVishnya <vishnyaostap88@gmail.com>
2019-10-24 15:58:59 +0300

    create back side

--- server/config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: "mongodb+srv://vishnya:Uahh9885@cluster0-lqbco.mongodb.net/test?retryWrites=true&w=majority",
-    jwt:'dev-jwt'
-}
--- server/routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require('../controllers/authentication')
-
-router.post("/login", controller.loginUser);
-router.post("/register", controller.registerUser);
-
-module.exports = router;
